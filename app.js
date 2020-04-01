5249e80607fd171f76cbf3746103665dabdd9066
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2020-04-01 19:41:30 +0530

    chore: Update dependencies and remove firebase config (#2)

--- app.js
+++ app.js
@@ -1,34 +1,6 @@
 var express = require("express");
 var app = express();
 app.use(express.static('public'))
-var firebase = require("firebase");
-var config = {
-	apiKey: "AIzaSyB2crF7oigfEGHgCHvkH-fOVVh6NxLXMKs",
-	authDomain: "food-up-amu.firebaseapp.com",
-	databaseURL: "https://food-up-amu.firebaseio.com",
-	storageBucket: "food-up-amu.appspot.com",
-  };
-firebase.initializeApp(config);
-
-const admin = require('firebase-admin');
-
-var serviceAccount = require('key.json');
-
-admin.initializeApp({
-credential: admin.credential.cert(serviceAccount)
-});
-
-var db = admin.firestore();
-  
-var docRef = db.collection('users').doc('alovelace');
-
-var setAda = docRef.set({
-  first: 'Ada',
-  last: 'Lovelace',
-  born: 1815
-});
-  
-  
 
 app.get("/", function(req, res){
 	res.send("Hi there!");
