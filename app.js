40926e48260f65e609d6145a5f83154d97e758d1
Prajjwal Nijhara <prajjwalnijhara@gmail.com>
2020-05-01 19:12:40 +0530

    Add custom css

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect(
 )
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 app.set("view engine", "ejs")
 
