0d1cb4ecd1866c80ad241a27bb77231458e5bf10
Pizzo15 <lpizzini7@gmail.com>
2021-10-02 12:46:33 +0200

    refactor: removed deprecated body-parser

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 //Requiring Dependencies//
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const passport = require("passport")
-const expressSession = require("express-session")
-const LocalStrategy = require("passport-local")
-const methodOverride = require("method-override")
-const foodData = require("./models/foodup")
-const flash = require("connect-flash")
-const Comment = require("./models/comment")
-const User = require("./models/user")
-const seedDB = require("./seeds")
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressSession = require('express-session');
+const LocalStrategy = require('passport-local');
+const methodOverride = require('method-override');
+const foodData = require('./models/foodup');
+const flash = require('connect-flash');
+const Comment = require('./models/comment');
+const User = require('./models/user');
+const seedDB = require('./seeds');
 
 //Requiring Routes//
-const commentRoutes = require("./routes/comments")
-const foodUpRoutes = require("./routes/FoodUp")
-const indexRoute = require("./routes/index")
+const commentRoutes = require('./routes/comments');
+const foodUpRoutes = require('./routes/FoodUp');
+const indexRoute = require('./routes/index');
 
 //seedDB()
 /* Commneting seedDB call for testing purpose
@@ -28,54 +27,53 @@ and that can be buggy for the application
 */
 
 //Using Dependencies
-mongoose.connect(
-	"mongodb://localhost:27017/foodup",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	}
-)
+mongoose.connect('mongodb://localhost:27017/foodup', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(__dirname + '/public'))
-app.use(methodOverride("_method"))
-app.use(expressSession({
-    secret: "slfsjfhkdshgkfdsh",
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+app.use(
+  expressSession({
+    secret: 'slfsjfhkdshgkfdsh',
     resave: false,
-    saveUninitialized: false
-}))
-
+    saveUninitialized: false,
+  })
+);
 
 app.use(flash());
 
 //Initializing Passport//
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs');
 
-
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	next();
-})
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  next();
+});
 
 //Using Routes//
-app.use(indexRoute)
-app.use(foodUpRoutes)
-app.use(commentRoutes)
+app.use(indexRoute);
+app.use(foodUpRoutes);
+app.use(commentRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("error");
-})
+  res.status(404).render('error');
+});
 
-app.listen(process.env.PORT || 3000, function(){
-	console.log("Food up Server Started at PORT: 3000")
-})
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Food up Server Started at PORT: 3000');
+});
