f195d4b951e16b0375c6b90d3c22444d7a7a4be3
Luca Pizzini <lpizzini7@gmail.com>
2021-10-04 15:18:51 +0200

    Refactored auth routes

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,31 @@ const passport = require('passport');
 const User = require('../models/user');
 
 //Index route//
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.render('landing');
 });
 
 //Auth routes//
-router.get('/register', function (req, res) {
+router.get('/register', (req, res) => {
   res.render('register');
 });
 
-router.post('/register', function (req, res) {
-  let newUser = new User({ username: req.body.username });
-  User.register(newUser, req.body.password, function (err, user) {
-    if (err) {
-      console.log(err);
-      return res.render('register');
-    }
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    let newUser = new User({ username });
+    await User.register(newUser, password);
+
     passport.authenticate('local')(req, res, function () {
-      res.redirect('/FoodUp');
+      return res.redirect('/FoodUp');
     });
-  });
+  } catch (err) {
+    console.log(err);
+    res.render('register');
+  }
 });
 
-router.get('/login', function (req, res) {
+router.get('/login', (req, res) => {
   res.render('login');
 });
 
@@ -36,10 +38,10 @@ router.post(
     successRedirect: '/FoodUp',
     failureRedirect: '/login',
   }),
-  function (req, res) {}
+  (req, res) => {}
 );
 
-router.get('/logout', function (req, res) {
+router.get('/logout', (req, res) => {
   req.logout();
   req.flash('success', 'Successfully LoggedOut');
   res.redirect('FoodUp');
