3bbdec5d462e61d509bf25025800850555b0f5cd
Luca Pizzini <lpizzini7@gmail.com>
2021-10-04 14:56:20 +0200

    Added nodemon dev dependency

--- routes/FoodUp.js
+++ routes/FoodUp.js
@@ -1,94 +1,101 @@
-const express = require("express")
-const router  = express.Router()
-const foodData = require("../models/foodup")
-const middleware = require("../middleware")
+const express = require('express');
+const router = express.Router();
+const foodData = require('../models/foodup');
+const middleware = require('../middleware');
 
 //FoodUp route//
 //SHOW route//
-router.get("/FoodUp", function(_req, res){
-	foodData.find({}, function(err, foodData){
-		if(err){
-			console.log(err)
-		}else{
-			res.render("food-up/index", {foodData: foodData});
-		}
-	})
-})
+router.get('/FoodUp', function (_req, res) {
+  foodData.find({}, function (err, foodData) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('food-up/index', { foodData: foodData });
+    }
+  });
+});
 
 //CREATE food route//
-router.get("/FoodUp/new", middleware.isLoggedIn, function(_req, res){
-	res.render("food-up/new")
-})
+router.get('/FoodUp/new', middleware.isLoggedIn, function (_req, res) {
+  res.render('food-up/new');
+});
 
-router.post("/FoodUp", middleware.isLoggedIn, function(req, res){
-	const name = req.body.name
-	const description = req.body.description
-	const image = req.body.image
-	const author = {
-		id: req.user._id,
-		username: req.user.username
-	}
-	const newfoodData = {
-		name: name, 
-		description: description, 
-		image:  image,
-		author: author
-	}
-	foodData.create(newfoodData, function(err, _newfoodData){
-		if(err){
-			console.log(err)
-		} else{
-			res.redirect("/FoodUp")
-		}
-	})
-})
+router.post('/FoodUp', middleware.isLoggedIn, function (req, res) {
+  const name = req.body.name;
+  const description = req.body.description;
+  const image = req.body.image;
+  const author = {
+    id: req.user._id,
+    username: req.user.username,
+  };
+  const newfoodData = {
+    name: name,
+    description: description,
+    image: image,
+    author: author,
+  };
+  foodData.create(newfoodData, function (err, _newfoodData) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/FoodUp');
+    }
+  });
+});
 
-router.get("/FoodUp/:id", function(req, res){
-	foodData.findById(req.params.id).populate("comments").exec(function(err, foundFoodData){
-		if(err){
-			console.log(err)
-		} else{
-			res.render("food-up/show", {foundFoodData: foundFoodData})
-		}
-	})
-})
+router.get('/FoodUp/:id', function (req, res) {
+  foodData
+    .findById(req.params.id)
+    .populate('comments')
+    .exec(function (err, foundFoodData) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('food-up/show', { foundFoodData: foundFoodData });
+      }
+    });
+});
 
 //EDIT food route//
-router.get("/FoodUp/:id/edit", middleware.isFoodPostOnwer, function(req, res){
-    foodData.findById(req.params.id, function(err, foundFoodData){
-		if(err){
-			res.redirect("/FoodUp/" + foodData.id)
-		} else{
-			res.render("food-up/edit", {foundFoodData: foundFoodData})
-		}
-	})
-})
+router.get('/FoodUp/:id/edit', middleware.isFoodPostOnwer, function (req, res) {
+  foodData.findById(req.params.id, function (err, foundFoodData) {
+    if (err) {
+      res.redirect('/FoodUp/' + foodData.id);
+    } else {
+      res.render('food-up/edit', { foundFoodData: foundFoodData });
+    }
+  });
+});
 
 //UPDATE ROUTE//
-router.put("/FoodUp/:id", middleware.isFoodPostOnwer, function(req, res){
-	const updatedFoodData = {
-		name: req.body.name,
-		description: req.body.description,
-		image: req.body.image
-	}
-    foodData.findByIdAndUpdate(req.params.id, updatedFoodData, function(err, updatedFoodData){
-        if(err){
-            res.redirect("/FoodUp")
-        } else{
-            res.redirect("/FoodUp/" + req.params.id)
-        }
-    })
-})
+router.put('/FoodUp/:id', middleware.isFoodPostOnwer, function (req, res) {
+  const updatedFoodData = {
+    name: req.body.name,
+    description: req.body.description,
+    image: req.body.image,
+  };
+  foodData.findByIdAndUpdate(
+    req.params.id,
+    updatedFoodData,
+    function (err, updatedFoodData) {
+      if (err) {
+        res.redirect('/FoodUp');
+      } else {
+        res.redirect('/FoodUp/' + req.params.id);
+      }
+    }
+  );
+});
 
 //DELETE ROUTE
-router.delete("/FoodUp/:id", middleware.isFoodPostOnwer, function(req, res){
-    foodData.findByIdAndRemove(req.params.id, function(err){
-        if(err){
-            console.log(err)
-        } else{
-            res.redirect("/FoodUp")
-        }
-    })
-})
+router.delete('/FoodUp/:id', middleware.isFoodPostOnwer, function (req, res) {
+  foodData.findByIdAndRemove(req.params.id, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/FoodUp');
+    }
+  });
+});
 
-module.exports = router
+module.exports = router;
--- routes/comments.js
+++ routes/comments.js
@@ -1,41 +1,39 @@
-const express = require("express")
-const router  = express.Router()
-const foodData = require("../models/foodup")
-const Comment = require("../models/comment")
-const middleware = require("../middleware")
+const express = require('express');
+const router = express.Router();
+const foodData = require('../models/foodup');
+const Comment = require('../models/comment');
+const middleware = require('../middleware');
 
 //Comments route//
-router.get("/FoodUp/:id/comments/new", middleware.isLoggedIn, function(req, res){
-	foodData.findById(req.params.id, function(err, foodData){
-		if(err){
-			console.log(err)
-		} else{
-			res.render("comments/new", {foodData: foodData})
-		}
-	})
-})
+router.get(
+  '/FoodUp/:id/comments/new',
+  middleware.isLoggedIn,
+  async (req, res) => {
+    try {
+      const foodData = await foodData.findById(req.params.id);
+      res.render('comments/new', { foodData });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
 
-router.post("/FoodUp/:id/comments", middleware.isLoggedIn,function(req, res){
-	foodData.findById(req.params.id, function(err, foodData){
-		if(err){
-			console.log(err)
-			res.redirect("/FoodUp")
-		}
-		else{
-			Comment.create(req.body.comment, function(err, comment){
-				if(err){
-					console.log(err)
-				} else{
-					comment.author.id = req.user._id
-					comment.author.username = req.user.username
-					comment.save()
-					foodData.comments.push(comment)
-					foodData.save()
-					res.redirect("/FoodUp/" + foodData.id)
-				}
-			})
-		}
-	})
-})
+router.post('/FoodUp/:id/comments', middleware.isLoggedIn, async (req, res) => {
+  try {
+    const foodData = await foodData.findById(req.params.id);
+    const comment = await Comment.create(req.body.comment);
+    comment.author.id = req.user._id;
+    comment.author.username = req.user.username;
+    await comment.save();
 
-module.exports = router
+    foodData.comments.push(comment);
+    await foodData.save();
+
+    res.redirect('/FoodUp/' + foodData.id);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/FoodUp');
+  }
+});
+
+module.exports = router;
